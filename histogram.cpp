07f65d38802f0e6d48234b68bb15227dc3d12cbc
Valentina <valentina@example>
2021-04-24 14:44:04 +0300

    cpp

---
+++ histogram.cpp
@@ -0,0 +1,19 @@
+#include "histogram.h"
+void
+find_minmax(const vector<double>& numbers, double& min, double& max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+}
+
