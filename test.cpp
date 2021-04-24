686b53bb1726e88c326bc0c4549130b0b623397a
Valentina <valentina@example>
2021-04-24 14:53:20 +0300

    test1

---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include "histogram.h"
+
+#include <cassert>
+
+void
+test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+void
+test_negative() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void
+test_odinakov() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+void
+test_odno() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+void
+test_pysto() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ }, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+int
+main() {
+    test_positive();
+    test_negative();
+
+}
