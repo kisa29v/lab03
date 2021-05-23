65b3389a8179dfd9d151878001c91b805d6f3ccd
kisa29v <57666261+kisa29v@users.noreply.github.com>
2021-05-23 21:41:54 +0300

    Add files via upload

--- histogram.h
+++ histogram.h
@@ -1,9 +1,15 @@
 #ifndef HISTOGRAM_H_INCLUDED
 #define HISTOGRAM_H_INCLUDED
+#pragma once
 #include <vector>
-
-
+#include <iostream>
 using namespace std;
+
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 void
 find_minmax(const vector<double>& numbers, double& min, double& max);
+vector<size_t> make_histogram(Input data);
 #endif // HISTOGRAM_H_INCLUDED
