b2db006622ff5d2cb9750f80b8d0210ba75f6bf4
kisa29v <57666261+kisa29v@users.noreply.github.com>
2021-05-23 21:41:26 +0300

    Add files via upload

--- histogram.cpp
+++ histogram.cpp
@@ -16,4 +16,22 @@ find_minmax(const vector<double>& numbers, double& min, double& max)
         }
     }
 }
+vector<size_t> make_histogram(Input data)
+{
+    double min = data.numbers[0];
+    double max = data.numbers[0];
+    find_minmax(data.numbers, min, max);
+    vector<size_t> bins(data.bin_count);
+    for (double number : data.numbers)
+    {
+        size_t bin = (size_t)((number - min) / (max - min) * data.bin_count);
+        if (bin == data.bin_count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
 
