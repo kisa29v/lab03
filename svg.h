c0c0f2888384cbcca3364f24014f771d9c8c8410
Yablokova <email>
2021-05-23 13:37:21 +0300

    mod

---
+++ svg.h
@@ -0,0 +1,18 @@
+#ifndef SVG_H_INCLUDED
+#define SVG_H_INCLUDED
+#include <vector>
+#include <iostream>
+using namespace std;
+void
+svg_begin(double width, double height);
+void
+svg_end();
+void
+svg_text(double left, double baseline, string text);
+void
+svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
+void
+show_histogram_svg(const vector<size_t>& bins);
+
+
+#endif // SVG_H_INCLUDED
