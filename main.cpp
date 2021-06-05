c06c6955135d736831909fd744b9ef85c0e9e736
Your Name <you@example.com>
2021-06-05 21:19:44 +0300

    printf

--- main.cpp
+++ main.cpp
@@ -17,7 +17,11 @@ input_numbers(istream& in, size_t count)
     return result;
 }
 int main()
-{
+{   const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567); // 01234567
+    return 0;
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -27,7 +31,8 @@ int main()
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-
+    double min,max;
+    find_minmax(numbers,min,max);
     const auto bins = make_histogram(numbers, bin_count);
 
     show_histogram_svg(bins);
