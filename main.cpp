406b1c26675b7c7e30d60bec78a9c28c56eaf9a9
Your Name <you@example.com>
2021-06-05 19:48:48 +0300

    strerror

--- main.cpp
+++ main.cpp
@@ -45,6 +45,10 @@ int main(int argc, char* argv[])
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
             curl_easy_cleanup(curl);
+            if (res){
+                cerr << curl_easy_strerror(res) << endl;
+                exit(1);
+                    }
                 }
                   }
     curl_global_init(CURL_GLOBAL_ALL);
