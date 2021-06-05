d776496c93d0e985e8da05740d8ca8bc20dbbbcc
Your Name <you@example.com>
2021-06-05 20:09:45 +0300

    var10

--- main.cpp
+++ main.cpp
@@ -47,13 +47,16 @@ write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
 }
 
 Input
-download(const string& address) {
+download(const string& address, bool verbose) {
     stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
     if(curl)
         {
             CURLcode res;
+            if (verbose){
+                curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+            }
             curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
@@ -67,18 +70,56 @@ download(const string& address) {
     curl_easy_cleanup(curl);
     return read_input(buffer, false);
         }
+Verb input_with_verb(int argc, char* argv[])
+{
+    Verb use;
+
+    use.verbose=false;
+    use.help=false;
+    use.urladress=0;
+
+    for (int i=1; i<argc; i++)
+    {
+        if (argv[i][0]=='-')
+        {
+            if (strcmp(argv[i], "-verbose") == 0)
+            {
+                use.verbose=true;
+            }
+            else
+            {
+                use.help=true;
+            }
+        }
+        else
+        {
+            use.urladress=argv[i];
+        }
+    }
+
+    return use;
+}
 int main(int argc, char* argv[])
 {   Input input;
-    if (argc > 1) {
-        input = download(argv[1]);
-    } else {
-        input = read_input(cin, true);
+    auto data=input_with_verb(argc,argv);
+        if (data.help)
+    {
+        cerr <<"For using CURLOPT_VERBOSE mode enter ''-verbose''" << "\n";
+        exit(2);
     }
+    if (data.urladress)
+    {
+        input=download(data.urladress,data.verbose);
+    }
+    else
+    {
+        input=read_input(cin, true);
 
     const auto bins = make_histogram(input);
 
     show_histogram_svg(bins);
     return 0;
+}
 }
 
 
