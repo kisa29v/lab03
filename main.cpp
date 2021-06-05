6d6c98fd1eb753232a7376e51698c1798d07eb0e
Your Name <you@example.com>
2021-06-05 23:29:30 +0300

    namecomp

--- main.cpp
+++ main.cpp
@@ -43,8 +43,12 @@ int main()
         DWORD version_major = version & mask_2;
         DWORD version_minor = version >> 8;
         DWORD build = platform;
-        printf("Windows v%u.%u (build %u)", version_major, version_minor, build);
+        printf("Windows v%u.%u (build %u)\n", version_major, version_minor, build);
     }
+    char computer_name[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD size = MAX_COMPUTERNAME_LENGTH+1;
+    GetComputerNameA(computer_name, &size);
+    printf("Computer name: %s\n", computer_name);
     return 0;
 }
 
