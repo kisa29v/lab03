9d8839e32c5fd5a6ec324d96b59401eb329e776e
Your Name <you@example.com>
2021-06-05 17:43:57 +0300

    lab4

--- histogram.cpp
+++ histogram.cpp
@@ -33,4 +33,38 @@ vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
     }
     return bins;
 }
+void
+show_histogram_text (const vector<size_t>& bins)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+ max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+ cout << ' ';
+        }
+        if (bin < 10) {
+ cout << ' ';
+        }
+ cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+ height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+ cout << '*';
+        }
+ cout << '\n';
+    }
+}
